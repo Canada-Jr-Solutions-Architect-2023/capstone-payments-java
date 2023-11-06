5aee524747a0e8c93e9beb78f2a2ec083e6ca649
LavanyaKanu <lavanya.kanukula@gmail.com>
2023-11-06 13:20:18 -0400

    Add authorization header to Feign call

--- src/main/java/com/customer/payments/feign/util/FeignServiceUtil.java
+++ src/main/java/com/customer/payments/feign/util/FeignServiceUtil.java
@@ -12,11 +12,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name="feign", url="https://carehub-quotes.ee-cognizantacademy.com/api")
 public interface FeignServiceUtil {
     @GetMapping("/policy/getPremiumForPayment/5")
-    public Double getPremiumForPayment();
+    public Double getPremiumForPayment(@RequestHeader(value = "Authorization", required = true) String authorizationHeader);
 
     @PutMapping("/policy/updatePaymentStatus")
     public ResponseEntity<String> updatePayment(@RequestBody PaymentRequest paymentRequest);
